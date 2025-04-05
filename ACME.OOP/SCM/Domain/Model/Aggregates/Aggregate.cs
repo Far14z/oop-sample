4a7ed2c2e29f191b3eef1f5a016aa008ea3cb918
Farid Coronel <faridce14@gmail.com>
2025-04-05 12:49:14 -0500

    feat(supplier): add aggregate

---
+++ ACME.OOP/SCM/Domain/Model/Aggregates/Aggregate.cs
@@ -0,0 +1,11 @@
+using ACME.OOP.Shared.Domain.model.ValueObjects;
+
+namespace ACME.OOP.SCM.Domain.Model.Aggregates;
+
+public class Aggregate(string identifier, string name, Address address)
+{
+    
+    public string Identifier { get; init; } = identifier ?? throw new ArgumentNullException(nameof(identifier));
+    public string Name { get; init; } = name ?? throw new ArgumentNullException(nameof(name));
+    public Address Address { get; init; } = address ?? throw new ArgumentNullException(nameof(address));
+}
