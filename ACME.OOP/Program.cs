aaa5670aa2d6674b5cb746b09e1c7db76b54c23a
Farid Coronel <faridce14@gmail.com>
2025-04-08 17:43:31 -0500

    feat(main):  main behavior.

--- ACME.OOP/Program.cs
+++ ACME.OOP/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ACME.OOP.Procurement.Domain.Model.Aggregates;
+using ACME.OOP.Procurement.Domain.Model.ValueObjects;
+using ACME.OOP.SCM.Domain.Model.Aggregates;
+using ACME.OOP.SCM.Domain.Model.ValueObjects;
+using ACME.OOP.Shared.Domain.model.ValueObjects;
 
-Console.WriteLine("Hello, World!");
+// Create a new Supplier object
+var supplierAddress = new Address("Main street", "123", "New York", "New York", "NY", "USA");
+var supplier = new Supplier("SUP001", "Microsoft, Inc.", supplierAddress);
+
+// Create a PurchaseOrder object
+var purchaseOrder = new PurchaseOrder("PO001", new SupplierId(supplier.Identifier), "USD");
+purchaseOrder.AddItem(ProductId.New(), 10, 25.99m);
+purchaseOrder.AddItem(ProductId.New(), 5, 15.99m);
+
+// Display the purchase order details
+Console.WriteLine($"Purchase Order ID: {purchaseOrder.OrderNumber} created on {purchaseOrder.OrderDate}" +
+                  $"for supplier {supplier.Name}");
+foreach (var item in purchaseOrder.Items)
+{
+    Console.WriteLine($"Item Subtotal: {item.CalculateSubTotal.Amount} {item.CalculateSubTotal.Currency}");
+}
+
+var total = purchaseOrder.CalculateTotal();
+Console.WriteLine($"Total: {total.AsString()} ");
