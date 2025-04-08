4baba5265b768273e380c4e3cd4663a4b1aca962
Farid Coronel <faridce14@gmail.com>
2025-04-08 16:15:50 -0500

    feat(supplier): update supplier value object

--- ACME.OOP/SCM/Domain/Model/ValueObjects/SupplierId.cs
+++ ACME.OOP/SCM/Domain/Model/ValueObjects/SupplierId.cs
@@ -3,11 +3,10 @@ namespace ACME.OOP.SCM.Domain.Model.ValueObjects;
 public record SupplierId
 {
     public string Identifier { get; init; }
-
     public SupplierId(string identifier)
     {
         if (string.IsNullOrWhiteSpace(identifier))
-            throw new ArgumentException("Supplier Id cannot be null or empty", nameof(identifier));
+            throw new ArgumentException("Supplier ID cannot be null or empty", nameof(identifier));
         Identifier = identifier;
             
     }
